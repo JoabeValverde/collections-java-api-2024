bf6e494e460120fe0fa4685e04581a14e5aa78d8
JoabeValverde <joabe23@hotmail.com>
2024-05-30 23:02:34 -0300

    Adicionado Ordenacao Produto e Cadastro de Produto
    Signed-off-by: JoabeValverde <joabe23@hotmail.com>

---
+++ src/set/Ordenacao/Produto.java
@@ -0,0 +1,78 @@
+package set.Ordenacao;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Produto implements Comparable<Produto> {
+    //atributos
+private long codigo;
+private String nome;
+private double preco;
+private int quantidade;
+
+// construtos
+
+    public Produto(long codigo, String nome, double preco, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    //metodo acessor Get
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public int compareTo(Produto p) {
+        return nome.compareToIgnoreCase(p.getNome());
+    }
+
+    // regra equal() and hashCode() para difereciar o codigo dos outros produtos
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto produto)) return false;
+        return getCodigo() == produto.getCodigo();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigo());
+    }
+
+    // To String
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
+//Criando um Comparator por preco
+class ComparatorPorPreco implements Comparator<Produto>{
+
+    @Override
+    public int compare(Produto p1, Produto p2) {
+        return Double.compare(p1.getPreco(), p2.getPreco());
+    }
+}
